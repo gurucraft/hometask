397c166e4d1a0bc5f76261eec32a3974d4e69100
guruCraft <svitlahata@gmail.com>
2014-10-23 10:38:20 +0300

    Add MVC, Spring date

--- src/main/java/com/hometask/controllers/GetPerson.java
+++ src/main/java/com/hometask/controllers/GetPerson.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 /**
  * Created by guru on 21.10.14.
  * (@)Svitla Hata
@@ -17,12 +19,9 @@ public class GetPerson {
     @Autowired
     private PersonalService personalService;
 
-    @RequestMapping(value = "/getAll",
-            method = RequestMethod.GET)
-    public
-    @ResponseBody
-    Person printHello() {
-        return personalService.getAll().get(0);
-
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    public @ResponseBody List<Person> printHello() {
+        return personalService.getAll();
     }
+
 }
